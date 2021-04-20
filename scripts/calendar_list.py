31af6626951202b5348188589291f2b6208d0808
tayutaedomo <tayutaedomo@gmail.com>
2021-04-20 18:24:00 +0900

    add: calendar_list.py

---
+++ scripts/calendar_list.py
@@ -0,0 +1,28 @@
+import os
+import sys
+
+sys.path.append(os.path.join(os.path.dirname(__file__)))
+
+from utils.api import get_calendar_service
+
+
+def main():
+    service = get_calendar_service()
+
+    page_token = None
+
+    while True:
+        calendar_list = service.calendarList().list(pageToken=page_token).execute()
+
+        for item in calendar_list['items']:
+            # print(item)
+            print(item['summary'], item['id'])
+
+        page_token = calendar_list.get('nextPageToken')
+
+        if not page_token:
+            break
+
+
+if __name__ == '__main__':
+    main()
