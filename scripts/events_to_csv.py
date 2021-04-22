7f806f6ba01e2729b7c9197a5ac252d872c085c0
tayutaedomo <tayutaedomo@gmail.com>
2021-04-22 20:43:19 +0900

    add: argparse handling

--- scripts/events_to_csv.py
+++ scripts/events_to_csv.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from datetime import datetime
+import argparse
 
 sys.path.append(os.path.join(os.path.dirname(__file__)))
 
@@ -8,30 +9,35 @@ from utils.events import Events
 
 
 def main():
-  if len(sys.argv) < 2:
-    print('Calendar ID is required.')
-    return
-
-  if len(sys.argv) < 4:
-    print('Start date and End date are required.')
-    print('Format: 2021-01-01T00:00:00+0900')
-    return
-
-  events = Events()
+  args = parse_args()
 
   params = {
-      'calendarId': sys.argv[1],
-      'timeMin': sys.argv[2],
-      'timeMax': sys.argv[3],
+      'calendarId': args.calendar,
+      'timeMin': args.min,
+      'timeMax': args.max,
   }
 
-  if len(sys.argv) > 4:
-    params['q'] = sys.argv[4]
+  if args.keyword:
+    params['q'] = args.keyword
 
+  events = Events()
   events.fetch(params)
 
   filename = datetime.now().strftime('%y%m%d_%H%M%S.csv')
   events.to_csv(filename)
+
+
+def parse_args():
+  parser = argparse.ArgumentParser(description='Fetch events and export to csv.')
+  parser.add_argument('calendar', type=str,
+                      help='Target calendar id of Google Calendar')
+  parser.add_argument('min', type=str,
+                      help='Min datetime for Query (e.g. 2021-01-01T00:00:00+0900)')
+  parser.add_argument('max', type=str,
+                      help='Max datetime for Query (e.g. 2021-01-01T00:00:00+0900)')
+  parser.add_argument('--keyword', type=str, help='Query keyword')
+
+  return parser.parse_args()
 
 
 if __name__ == '__main__':
