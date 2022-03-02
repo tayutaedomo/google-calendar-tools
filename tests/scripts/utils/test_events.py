2bc1db1c8efb9b777570fbe13b01cf822c0b282d
tayutaedomo <tayutaedomo@gmail.com>
2022-03-03 00:11:47 +0900

    Add test_events_fetch()

--- tests/scripts/utils/test_events.py
+++ tests/scripts/utils/test_events.py
@@ -1,8 +1,9 @@
 from __future__ import annotations
 
+from typing import Any
 from unittest.mock import Mock, patch
 
-from utils.events import CreateEventsInput, create_events
+from utils.events import CreateEventsInput, Events, create_events
 
 
 @patch("utils.events.insert_event", return_value={})
@@ -37,3 +38,61 @@ def test_create_events_with_weekday(mock_insert_event: Mock) -> None:
     responses = create_events(input_)
     assert len(responses) == 1
     mock_insert_event.assert_called_once()
+
+
+EXPECTED_RESPONSE: dict[str, Any] = {
+    "items": [
+        {
+            "status": "confirmed",
+            "summary": "item 1",
+            "start": {
+                "dateTime": "2022-02-01T09:00:00+09:00",
+                "timeZone": "Asia/Tokyo",
+            },
+            "end": {
+                "dateTime": "2022-02-01T09:30:00+09:00",
+                "timeZone": "Asia/Tokyo",
+            },
+        },
+        {
+            "status": "confirmed",
+            "summary": "item 2",
+            "start": {
+                "dateTime": "2022-02-01T09:30:00+09:00",
+                "timeZone": "Asia/Tokyo",
+            },
+            "end": {
+                "dateTime": "2022-02-01T09:45:00+09:00",
+                "timeZone": "Asia/Tokyo",
+            },
+        },
+        {
+            "status": "cancelled",
+            "summary": "item cancelled",
+            "start": {
+                "dateTime": "2022-02-01T10:00:00+09:00",
+                "timeZone": "Asia/Tokyo",
+            },
+            "end": {
+                "dateTime": "2022-02-01T10:30:00+09:00",
+                "timeZone": "Asia/Tokyo",
+            },
+        },
+    ]
+}
+
+
+@patch("utils.events.fetch_events", return_value=EXPECTED_RESPONSE)
+def test_events_fetch(mock_fetch_events: Mock) -> None:
+    events = Events()
+    params = {
+        "calendarId": "primary",
+        "timeMin": "2022-02-01T00:00:00+0900",
+        "timeMax": "2022-02-02T00:00:00+0900",
+    }
+    events.fetch(params)
+    assert len(events.items) == 3
+
+    expected_item: dict[str, Any] = EXPECTED_RESPONSE["items"][0]
+    item: dict = events.items[0]
+    assert item["summary"] == expected_item["summary"]
