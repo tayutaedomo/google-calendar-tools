ad9e6a4a7f46d25c39241e43d472468f067ade52
tayutaedomo <tayutaedomo@gmail.com>
2022-03-09 02:25:17 +0900

    Add logging.yml

--- scripts/utils/logging.py
+++ scripts/utils/logging.py
@@ -1,11 +1,12 @@
 import os
-from json import load
 from logging import config
+
+import yaml
 
 
 def setup_logger() -> None:
     config_path = os.path.join(
-        os.path.dirname(__file__), "..", "config", "logging.json"
+        os.path.dirname(__file__), "..", "config", "logging.yaml"
     )
     with open(config_path, "r", encoding="utf-8") as f:
-        config.dictConfig(load(f))
+        config.dictConfig(yaml.load(f.read(), Loader=yaml.SafeLoader))
